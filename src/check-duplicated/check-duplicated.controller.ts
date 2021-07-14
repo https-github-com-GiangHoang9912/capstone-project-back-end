104bfdd78e83c938f92d1cd42040ca9261268b42
Giang Hoang <gianghoang.9955@gmail.com>
2021-07-14 08:52:08 +0700

    update training data duplicated

--- src/check-duplicated/check-duplicated.controller.ts
+++ src/check-duplicated/check-duplicated.controller.ts
@@ -10,11 +10,17 @@ import {
   HttpStatus,
   Req,
   UseGuards,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { QuestionCheckDuplicatedDto } from 'src/dto/check-duplicated.dto';
 import { CheckDuplicatedService } from 'src/services/check-duplicated.service';
 import { Response, Request } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 import * as CONSTANTS from '../constant';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import * as path from 'path';
 
 @UseGuards(JwtAuthGuard)
 @Controller('check-duplicated')
@@ -22,7 +28,7 @@ export class CheckDuplicatedController {
   constructor(
     private readonly checkDuplicatedService: CheckDuplicatedService,
     private readonly historyService: HistoryService,
-    private readonly authService: AuthService
+    private readonly authService: AuthService,
   ) {}
 
   @Post('/')
@@ -32,14 +38,43 @@ export class CheckDuplicatedController {
     @Res() res: Response,
   ): Promise<any> {
     try {
-      const user = this.authService.verifyToken(req.cookies.token.jwt_token)
-      await this.historyService.createHistory(CONSTANTS.HISTORY_TYPE.DUPLICATE, questions.question, user.sub)
+      const user = this.authService.verifyToken(req.cookies.token.jwt_token);
+      await this.historyService.createHistory(
+        CONSTANTS.HISTORY_TYPE.DUPLICATE,
+        questions.question,
+        user.sub,
+      );
       const data = await this.checkDuplicatedService.checkDuplicated(
         questions.question,
       );
       return res.status(HttpStatus.OK).send(data.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
+    }
+  }
+
+  @Post('/upload-dataset')
+  @UseInterceptors(
+    FileInterceptor('train', {
+      storage: diskStorage({
+        destination: './uploads/datasets',
+        filename: (req, file, cb) => {
+          const filename: string =
+            path.parse(file.fieldname).name.replace(/\s/g, '');
+          const extension: string = path.parse(file.originalname).ext;
+
+          cb(null, `${filename}${extension}`);
+        },
+      }),
+    }),
+  )
+  async uploadDataset(@UploadedFile() file, @Res() res: Response,): Promise<any> {
+    try {
+      // function to train 
+      res.status(HttpStatus.OK).send("training successful ... !")
+      return
+    } catch (error) {
+      console.log(error);
     }
   }
 }
