02635fea6fb48f83eec006b7b8ca74d72b42d53b
Giang Hoang <gianghoang.9955@gmail.com>
2021-08-20 14:01:01 +0700

    update train data file csv

--- src/entities/question-bank.entity.ts
+++ src/entities/question-bank.entity.ts
@@ -9,13 +9,25 @@ import {
   JoinColumn,
   ManyToOne,
 } from 'typeorm';
+import {
+  ExtendedColumnOptions,
+  AutoEncryptSubscriber,
+} from 'typeorm-encrypted';
 
 @Entity('question_bank')
 export class QuestionBank extends BaseEntity {
   @PrimaryGeneratedColumn('increment')
   id: number;
 
-  @Column({ name: 'question_text' })
+  @Column(<ExtendedColumnOptions>{
+    name: 'question_text',
+    encrypt: {
+      key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
+      algorithm: 'aes-256-cbc',
+      ivLength: 16,
+      iv: 'ff5ac19190424b1d88f9419ef949ae56',
+    },
+  })
   questionText: string;
 
   @Column({ name: 'subject_id' })
